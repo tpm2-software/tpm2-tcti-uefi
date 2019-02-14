069b40996f7d6ded656cb2c2910deae2603e7034
Philip Tricca <philip.b.tricca@intel.com>
2019-02-14 11:17:55 -0800

    docs: Remove swtpm & qemu example scripts.
    Now that we have automated the swtpm + qemu + ovmf usage in the test
    harness there's not need to keep these example scripts around. Instead
    the docs refer readers to this script for the best / tested example we
    have.
    
    Signed-off-by: Philip Tricca <philip.b.tricca@intel.com>

--- lib/swtpm2.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-TMP_DIR=${TMP_DIR:-"/tmp/swtpm2"}
-mkdir -p ${TMP_DIR}
-swtpm socket --tpm2 \
-    --tpmstate dir=${TMP_DIR} \
-    --ctrl type=unixio,path=${TMP_DIR}/tpm-sock \
-    --log level=20 2>&1 | tee ${TMP_DIR}/swtpm.log
